acc47c30988d3a050d8b1bca12c5328cc94c1dfa
ChrisGoldswain <120228798+ChrisGoldswain@users.noreply.github.com>
2023-03-31 13:22:53 +0200

    Initial Commit

---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,43 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Christopher Goldswain on 01.02.23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    var temperatureString: String {
+        return String(format: "%.1f", temperature)
+    }
+    
+    var conditionName: String {
+        switch conditionId {
+        case 200...232 :
+            return "cloud.bolt.rain"
+        case 300...321 :
+            return "cloud.drizzle"
+        case 500...531 :
+            return "cloud.rain"
+        case 600...622 :
+            return "cloud.snow"
+        case 701...781 :
+            return "cloud.fog"
+        case 800 :
+            return "sun.max"
+        case  801...804 :
+            return "cloud"
+        default :
+            return "sun.max"
+        }
+    }
+    
+
+}
